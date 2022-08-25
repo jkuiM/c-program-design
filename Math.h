6d094da9fcbaa410b0887ae723713bb1354448b9
jkuiM <64448969+jkuiM@users.noreply.github.com>
2022-08-25 13:58:59 +0800

    feature add Match.c

---
+++ Math.h
@@ -0,0 +1,8 @@
+
+
+#ifndef C_PROGRAM_DESIGN_MATH_H
+#define C_PROGRAM_DESIGN_MATH_H
+
+int sum(int x, int y);
+
+#endif //C_PROGRAM_DESIGN_MATH_H
--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "Math.h"
+#include "Math.c"
 
 int main() {
     printf("Hello, World!\n");
+    int a = sum(1,2);
+    printf("%d",a);
     return 0;
 }
