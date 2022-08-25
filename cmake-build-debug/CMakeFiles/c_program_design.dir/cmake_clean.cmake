1622d17ba72e874c1ed0691dd923d30a7cc62915
jkuiM <64448969+jkuiM@users.noreply.github.com>
2022-08-25 08:25:25 +0800

    feature add project

---
+++ cmake-build-debug/CMakeFiles/c_program_design.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_program_design.dir/main.c.obj"
+  "c_program_design.exe"
+  "c_program_design.exe.manifest"
+  "c_program_design.pdb"
+  "libc_program_design.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_program_design.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
